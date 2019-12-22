3c35fe302599dbe49ac092b75361f9e53d4519fe
shntsdmr <shntsdmr@icloud.com>
2019-12-22 16:05:43 +0300

    first commit

---
+++ if-elif-else.py
@@ -0,0 +1,29 @@
+if 0:
+    print("Bu adımı direk geçer")
+elif "True":
+    print("Bu adım doğru olduğu için burda durur")
+elif "":
+    print("üstteki adım doğru olduğu için buraya gelmez")
+
+if 1:
+    print("Bu adım doğru olduğu için burda durur")
+elif "sahin":
+    print("Adım doğru fakat üstteki doğru olduğu için çalışmaz")
+
+if 0:
+    print("bu adım yanlış ve geçer")
+elif "":
+    print("bu adım yanlış ve sonraki adıma geçer")
+elif ():
+    print("bu adım yanlış ve sonraki adıma geçer")
+elif "tuba":
+    print("Bu adım doğru olduğu için burda durur")
+
+if 0:
+    print("bu adım yanlış ve sonraki adıma geçer")
+elif "":
+        print("bu adım yanlış ve sonraki adıma geçer")
+if 0:
+    print("Bu adım doğru olduğu için burda durur")
+else:
+    print("bu adım çalışır")
